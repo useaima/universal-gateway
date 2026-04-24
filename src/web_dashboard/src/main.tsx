ebd8007ac97580ddce87f667ee1334aee934500d
Alvins Mukabane <mukabanealvins@gmail.com>
2026-04-25 01:38:48 +0300

    chore: clean up production code for release

--- src/web_dashboard/src/main.tsx
+++ src/web_dashboard/src/main.tsx
@@ -3,16 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-console.log("UTG: Main entry point execution started.");
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  console.log("UTG: Root element found, starting React render.");
   createRoot(rootElement).render(
     <StrictMode>
       <App />
     </StrictMode>,
   );
-} else {
-  console.error("UTG: Root element NOT found!");
 }
