cb3957f6970120a1b446f5b008703c85b164c7aa
Alvins-mukabane <mukabanealvins@gmail.com>
2026-04-30 03:50:02 +0300

    Fix missing div tag in Hero component

--- src/web_dashboard/src/components/Hero.tsx
+++ src/web_dashboard/src/components/Hero.tsx
@@ -176,6 +176,7 @@ export default function Hero({ onOpenAuth }: HeroProps) {
                       </div>
                     </div>
                   ))}
+                </div>
               </div>
             </div>
           </div>
